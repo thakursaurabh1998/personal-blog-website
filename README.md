74dc7858c8ff7458e1a8c76ee4510b46aa79980b
Saurabh Thakur <thakursaurabh1998@gmail.com>
2020-02-02 19:39:45 +0530

    initial setup commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Saurabh Thakur
+
+> Personal website and blog [www.saurabhthakur.dev](https://www.saurabhthakur.dev)
+
+## Deploys
+
+[![Netlify](https://www.netlify.com/img/global/badges/netlify-color-accent.svg)](https://app.netlify.com/sites/thakursaurabh1998)
+Commits to master and PRs are automatically built and deployed by Netlify
---
+++ gatsby-config.js
@@ -0,0 +1,76 @@
+require('dotenv').config({
+  path: '.env',
+});
+
+module.exports = {
+  siteMetadata: {
+    siteTitle: 'Saurabh Thakur',
+    siteTitleAlt: 'Saurabh Thakur',
+    author: '@thakursaurabh98',
+    siteImage: '/ok.png',
+    siteHeadline: '',
+    siteUrl: 'https://saurabhthakur.dev',
+    siteDescription: '',
+    siteLanguage: 'en',
+  },
+  plugins: [
+    {
+      resolve: './theme',
+      options: {
+        navigation: [
+          {
+            title: 'Blog',
+            slug: '/blog',
+          },
+          {
+            title: 'About',
+            slug: '/about',
+          },
+        ],
+        externalLinks: [
+          {
+            name: 'Twitter',
+            url: 'https://twitter.com/thakursaurabh98',
+          },
+          {
+            name: 'Instagram',
+            url: 'https://www.instagram.com/thakursaurabh98',
+          },
+        ],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Saurabh Thakur - Blog',
+        short_name: 'minimal-blog',
+        description: 'Blog and about page',
+        start_url: '/',
+        background_color: '#fff',
+        theme_color: '#6B46C1',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    },
+    'gatsby-plugin-offline',
+    'gatsby-plugin-netlify',
+    'gatsby-plugin-sitemap',
+  ],
+};
